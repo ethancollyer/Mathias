dc48ed0dd38622b1a7188a86ee7033bd0b4f25c8
ethan <ethan.l.collyer@gmail.com>
2024-11-16 13:24:47 -0800

    merged agent classes into agent.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from modules.eq_agent import EqAgent
-from modules.declarative_agent import DeclarativeAgent
+from modules.agent import Agent
 import json
 
 
 with open("questions.json", "r") as fp:
     questions = json.load(fp)
 
-declarative_agent = DeclarativeAgent()
-equation_agent = EqAgent()
-question = questions['question_2']
+with open("sys_messages.json", "r") as fp:
+    parser_sys_message = json.load(fp)['sys_message_parser']
+    eq_sys_message = json.load(fp)['sys_message_eq']
 
-declarative_agent.append_history(input_message=question['question'])
-response = declarative_agent.chat()
+parser_agent = Agent(sys_message=parser_sys_message)
+equation_agent = Agent(sys_message=eq_sys_message)
+question = questions['question_1']
+
+parser_agent.append_history(input_message=question['question'])
+response = parser_agent.chat()
 print(f"EXPLINATION:\n{response}\n{'='*50}")
 
 equation_agent.append_history(input_message=response)
---
+++ modules/agent.py
@@ -0,0 +1,24 @@
+import ollama
+
+
+class Agent(ollama.Client):
+    def __init__(self, sys_message: str, model: str = 'mistral:7b-instruct'):
+        super().__init__()
+        self.model = model
+        self.sys_message = {
+            "role": "system",
+            "content": sys_message
+        }
+        self.history = [self.sys_message]
+
+    def append_history(self, input_message: str, role: str = 'user'):
+        message = {
+            "role": role,
+            "content": input_message
+        }
+        self.history.append(message)
+
+    def chat(self):
+        response = super().chat(model=self.model, messages=self.history)
+        return response['message']['content']
+    
