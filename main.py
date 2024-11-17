2425fb0e438d6ee997ac9d3127cb27605cbe25db
ethan <ethan.l.collyer@gmail.com>
2024-11-16 16:39:38 -0800

    updated calculator.py file to be a class with functions to simplify and solve problems

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from modules.agent import Agent
+from modules.calculator import Calculator
 import json
 
 
@@ -6,12 +7,11 @@ with open("questions.json", "r") as fp:
     questions = json.load(fp)
 
 with open("sys_messages.json", "r") as fp:
-    parser_sys_message = json.load(fp)['sys_message_parser']
-    eq_sys_message = json.load(fp)['sys_message_eq']
+    sys_messages = json.load(fp)
 
-parser_agent = Agent(sys_message=parser_sys_message)
-equation_agent = Agent(sys_message=eq_sys_message)
-question = questions['question_1']
+parser_agent = Agent(sys_message=sys_messages["sys_message_parser"])
+equation_agent = Agent(sys_message=sys_messages["sys_message_eq"])
+question = questions['question_2']
 
 parser_agent.append_history(input_message=question['question'])
 response = parser_agent.chat()
@@ -20,6 +20,10 @@ print(f"EXPLINATION:\n{response}\n{'='*50}")
 equation_agent.append_history(input_message=response)
 response = equation_agent.chat()
 print(f"RESPONSE:\n{response}")
+
+calculator = Calculator(function=response)
+output = calculator.calulate()
+print(f"CALCULATED:\n{output}")
 
 print(f"ANSWER:\n{question['answer']}")
 
--- modules/calculator.py
+++ modules/calculator.py
@@ -1,15 +1,64 @@
-import sympy
+from sympy import Eq, sympify, symbols, simplify, solve
+import math
 import re
 
 
-def parse_func(function: str):
-    pattern = re.compile(r"[A-Za-z]")
-    matches = re.findall(pattern, function)
-    return sympy.sympify(function)
+class Calculator():
+    def __init__(self, function: str):
+        self.function = function
 
-def calulate(function):
-    return function
+    def split(self):
+        right = self.function
+        expressions = list()
+        expression_count = self.function.count("=")
+        if expression_count > 0:
+            while expression_count > 0:
+                left = right[:right.find("=")]
+                right = right[right.find("=") + 1:]
+                expressions.append(left)
 
-func = parse_func("a = (5 - 2) + (5 - 2)")
+                expression_count = right.count("=")
+                if expression_count == 0:
+                    expressions.append(right)
+        else:
+            expressions.append(right)
 
-print(func)
+        return expressions
+
+    def symbolfy(self):
+        self.expressions = self.split()
+        variables = set()
+        pattern = re.compile(r"(?<![A-Za-z])[A-Za-z](?![A-Za-z])")
+        for expression in self.expressions:
+            matches = re.findall(pattern, expression)
+            if matches:
+                variables.update({match for match in matches})
+        
+        return symbols(variables)
+    
+    def simplify(self):
+        self.symbols = self.symbolfy()
+        expressions_simplified = list()
+        for expression in self.expressions:
+            expressions_simplified.append(simplify(sympify(expression)))
+        
+        return expressions_simplified
+
+
+    def calulate(self):
+        self.expressions_simplified = self.simplify()
+        expression_count = len(self.expressions_simplified)
+        output = ""
+        if expression_count > 1:
+            for i in range(expression_count - 1):
+                left = self.expressions_simplified[i]
+                right = self.expressions_simplified[i + 1]
+                equation = Eq(left, right)
+                calc = solve(equation)
+                output += f"Calculation for {equation}:\n {calc}\n\n"
+        else:
+                equation = Eq(self.expressions_simplified[0], 0)
+                calc = solve(equation)
+                output += f"Calculation for {equation}:\n {calc}\n\n"
+        return output
+
