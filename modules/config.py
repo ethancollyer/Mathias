6c7a7ff3f6241a73021e92ec30de53c93fb1d8c2
ethancollyer <ethan.l.collyer@gmail.com>
2024-11-23 22:02:42 -0800

    adding the config and client that were not in previous commit

---
+++ modules/config.py
@@ -0,0 +1,64 @@
+
+
+def get_prompt(prompt_type: str = "calc"):
+    """Retrieves either 'calc', 'client', or 'debugger' prompt."""
+    if "calc" in prompt_type:
+        return calc_prompt
+    else:
+        return client_prompt
+
+client_prompt = """"
+You are a problem solver specializing in structured reasoning. Your task is to translate word problems into logical steps and mathematical equations. Do not perform any math. **Follow These Steps:**
+
+1. Identify and clearly define variables.
+2. Translate each logical relationship into a mathematical equation.
+3. Define the final equation that will solve the problem.
+
+**Instructions:**
+-> Use symbols like 'a', 'c', or 'tA', 'tB' to define variables.
+-> Write equations compatible with the Python sympy library.
+-> Ensure each equation includes at least one variable representing the unknown to be solved.
+-> For multi-variable or ambiguous problems, provide reasonable assumptions, justify them, and document them clearly within the steps.
+-> Substitute all known variables with their respective values for the final equation.
+-> Do not perform any math. Only define the varibles and equations needed to solve the problem.
+
+**Example Input Problem:**
+A train leaves Station A traveling at 80 km/h. Another train leaves Station B, 120 km away, traveling at 100 km/h toward Station A. When will the trains meet?
+
+**Example Output:**
+Define Variables: 
+    - dA = distance in km traveled by trian A after traveling for t hours.
+    - dB = distance in km traveled by trian B after traveling for t hours.
+    - d = distance in km between station A and station B.
+    - t = time, in hours, it takes for the trains to meet.
+Relationship Equations:
+1. The trains are traveling toward each other, and the total distance they must cover together is 120 km. Therefore, the sum of thier distances equals 120 km:
+    - dA + dB = 120
+2. The train from station A travels at 80 km/h for t hours:
+    - dA = 80*t
+3. The train from station B travels at 100 km/h for t hours:
+    - dB = 100*t
+Translate into Final Equation:
+- Substitute the expressions for dA and dB into the total distance equation:
+    - 80*t + 100*t = 120
+Final Equation:
+- The final equation to solve for t is:
+    - 80*t + 100*t = 120
+
+Use this framework to approach each problem systematically.
+"""
+
+calc_prompt = """"
+You are an LLM designed to solve numerical problems. You will receive a series of steps to solve a problem. **Your Task:**
+
+1. Interpret the steps carefully. Ensure you understand the calculations required at each step.
+2. Ensure the equations are compatible with the Python sympy library. e.g. sin^(-1)(2) should be written as asin(2). And 3x should be written as 3*x.
+3. Use the provided calculator tool to perform any necessary arithmetic or calculations.
+
+**Example Input Equation:**
+x^2 / 4 = arctan(16)
+
+**Example Output Equation for Calculator Tool:**
+x**2 / 4 = atan(16)
+"""
+
---
+++ modules/openai_client.py
@@ -0,0 +1,100 @@
+from .helpers import *
+from .config import get_prompt
+from .calculator import Calculator
+import openai
+import os
+
+
+class OpenaiClient:
+    def __init__(self, model: str):
+        self.model = model
+        self.tools = get_tools()
+        self.tool_choice = {"type": "function", "function": {"name": "calculate"}}
+        self.sys_message = {"role": "system", "content": get_prompt(prompt_type="client")}
+        self.history = [self.sys_message]
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    def chat(self):
+        """Streams the final response from the client"""
+        self.history[0] = {"role": "system", "content": get_prompt(prompt_type="client")}
+        response = openai.chat.completions.create(
+            model=self.model,
+            messages=self.history,
+            tools=self.tools,
+            tool_choice="none",
+            stream=True
+        )
+
+        for chunk in response:
+            content = chunk.choices[0].delta.content
+            if content:
+                print(content, end="", flush=True)
+
+        self.history = [self.sys_message]
+
+
+    def update_history(self, input_message: str):
+        """Updates client history after the user sends a message to the client"""
+        self.history.append({"role": "user", "content": input_message})
+        self.setup()
+        self.history[0] = {"role": "system", "content": get_prompt(prompt_type="calc")}
+        completion = self.get_completion(tool_choice=self.tool_choice)
+        self.args = self.get_completion_args(completion)
+        message = self.get_message(completion)
+        message["content"] = self.get_content(completion, message)
+        self.history.append(message)
+
+        if completion.tool_calls:
+            self.history.extend(message for message in self.get_tool_messages(self.history))
+
+    def setup(self):
+        """Prepares logic for llm calculation"""
+        completion = self.get_completion(tool_choice="none")
+        message = self.get_message(completion)
+        self.history.append(message)
+
+    def get_completion(self, tool_choice):
+        """Retrieves the client message metadata"""
+        response = openai.chat.completions.create(
+            model=self.model,
+            messages=self.history,
+            tools=self.tools,
+            tool_choice=tool_choice
+        )
+
+        return response.choices[0].message
+
+    def get_message(self, completion):
+        "Parses the message metadata into a python dict"
+        return json.loads(completion.model_dump_json())
+
+    def get_content(self, completion, message: dict):
+        """Retrieves the content of the message"""
+        if completion.tool_calls:
+            return str(message["tool_calls"][0]["function"])
+        else:
+            return str(message["content"])
+
+    def get_tool_messages(self, history: list):
+        "Aggregates all tool messages into a list"
+        messages = []
+        for i in range(len(history)):
+            if "tool_calls" in history[i].keys() and history[i]["tool_calls"]:
+                for j in range(len(history[i]["tool_calls"])):
+                    message = {
+                        "role": "tool",
+                        "tool_call_id": history[i]["tool_calls"][j]["id"],
+                        "name": history[i]["tool_calls"][j]["function"]["name"],
+                        "content": Calculator(history[i]["tool_calls"][j]["function"]["arguments"]).calculate()
+                    }
+                    messages.append(message)
+
+        return messages
+
+    def get_completion_args(self, completion):
+        """Retrieves the input arguments identified by the client and to be used in the function call"""
+        if completion.tool_calls:
+            return [f"{json.loads(completion.tool_calls[i].function.arguments)}" for i in range(len(completion.tool_calls))]
+        else:
+            return None
+
